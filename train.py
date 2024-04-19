ece7776089239dd5820d297f92a0691ba9a44114
Ashelee <43794317+LiZhuoHong@users.noreply.github.com>
2024-04-19 14:17:34 +0800

    Update train.py

--- train.py
+++ train.py
@@ -15,7 +15,6 @@ parser.add_argument('--dataset', type=str, default='Chesapeake', help='experimen
 parser.add_argument('--max_epochs', type=int, default=100, help='maximum epoch number to train')
 parser.add_argument('--batch_size', type=int, default=16, help='batch_size per gpu')
 parser.add_argument('--base_lr', type=float,  default=0.01, help='segmentation network learning rate')
-parser.add_argument('--img_size', type=int, default=312, help='input patch size of network input')
 parser.add_argument('--seed', type=int, default=1234, help='random seed')
 parser.add_argument('--CNN_width', type=int, default=64, help='L2HNet_width_size, default is 64: light mode. Set to 128: normal mode')
 parser.add_argument('--savepath', type=str)
@@ -24,6 +23,8 @@ args = parser.parse_args()
 
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
 if __name__ == "__main__":
+    vit_patches_size=16
+    img_size=224
     cudnn.benchmark = True
     cudnn.deterministic = False
     random.seed(args.seed)
@@ -33,7 +34,7 @@ if __name__ == "__main__":
     dataset_name = args.dataset
     dataset_config = {
         'Chesapeake': {  # default dataset as a example
-            'list_dir': './dataset/CSV_list/Chesapeake_NewYork.csv', # The path of the *.csv file
+            'list_dir': '/home/ashelee/project_TransUNet/list/NY_raw.csv', # The path of the *.csv file
             'num_classes': 17
         }
     }# Create a config to your own dataset here
@@ -47,8 +48,7 @@ if __name__ == "__main__":
         os.makedirs(snapshot_path)
     config_vit = CONFIGS_ViT_seg["ViT-B_16"]
     config_vit.n_classes = args.num_classes
-    vit_patches_size=16
-    config_vit.patches.grid = (int(args.img_size / vit_patches_size), int(args.img_size / vit_patches_size))
-    net = ViT_seg(config_vit, backbone=L2HNet(width=args.CNN_width),img_size=args.img_size, num_classes=config_vit.n_classes).cuda()
+    config_vit.patches.grid = (int(img_size / vit_patches_size), int(img_size / vit_patches_size))
+    net = ViT_seg(config_vit, backbone=L2HNet(width=args.CNN_width),img_size=img_size, num_classes=config_vit.n_classes).cuda()
     net.load_from(weights=np.load(config_vit.pretrained_path))
     trainer_dataset(args, net, snapshot_path)
